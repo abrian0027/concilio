48ab7ade2f12c543f6c1a4e439f75280cdb544cb
Milton Abrian <abrian0027@gmail.com>
2026-02-09 23:12:49 -0400

    Incluir vendor con PHPMailer para produccion

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'concilio/sistema-membresia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a77ba5f4e51db9232f40c16c0e20fe9e84af6066',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'concilio/sistema-membresia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a77ba5f4e51db9232f40c16c0e20fe9e84af6066',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
